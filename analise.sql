80ec7a80d2c6eabeaa2d733426c843841aad6f5c
Luciana Moreira <lucrismoreira298@gmail.com>
2026-03-26 12:37:25 -0300

    Add ordering to sales queries and new summaries

--- analise.sql
+++ analise.sql
@@ -1,9 +1,21 @@
--- Total por cliente
+-- Total de vendas por cliente
 SELECT cliente, SUM(valor) AS total_gasto
 FROM vendas
-GROUP BY cliente;
+GROUP BY cliente
+ORDER BY total_gasto DESC;
 
 -- Produto mais vendido
 SELECT produto, COUNT(*) AS quantidade
 FROM vendas
-GROUP BY produto;
+GROUP BY produto
+ORDER BY quantidade DESC;
+
+-- Total de vendas por dia
+SELECT data, SUM(valor) AS total_dia
+FROM vendas
+GROUP BY data
+ORDER BY data;
+
+-- Total geral de vendas
+SELECT SUM(valor) AS total_geral
+FROM vendas;
